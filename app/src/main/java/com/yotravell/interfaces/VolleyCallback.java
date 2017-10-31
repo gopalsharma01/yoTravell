d6e933cedabf7ea10653eb35703fa0f244010d2c
Gopal Sharma <gopalsharma01@gmail.com>
2017-10-31 17:46:16 +0530

    (31-10-2017) changes in Volley Interface file

--- app/src/main/java/com/yotravell/interfaces/VolleyCallback.java
+++ app/src/main/java/com/yotravell/interfaces/VolleyCallback.java
@@ -1,9 +1,8 @@
 package com.yotravell.interfaces;
 
-import java.util.Map;
 
 /**
- * Created by Khatushyam on 10/31/2017.
+ * Created by Developer on 10/31/2017.
  */
 
 public interface VolleyCallback {
